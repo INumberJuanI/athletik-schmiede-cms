5fac7c78ba3416115fba4101c04a684dc567636f
Juan Canales <41006075+INumberJuanI@users.noreply.github.com>
2025-09-29 13:15:05 +0000

    db init2

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250929_131454 from './20250929_131454';
+
+export const migrations = [
+  {
+    up: migration_20250929_131454.up,
+    down: migration_20250929_131454.down,
+    name: '20250929_131454'
+  },
+];
