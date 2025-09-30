884ecdb08274ce380ba55e51500e508e3543fed0
Juan Canales <41006075+INumberJuanI@users.noreply.github.com>
2025-09-30 14:42:44 +0000

    header and hero adjustments

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -31,7 +31,7 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
 
   return (
     <header className="relative z-20 sticky top-0 w-full bg-white shadow-md transition">
-      <div className="flex justify-between">
+      <div className="container flex justify-between">
         <Link href="/">
           <Logo loading="eager" priority="high" className="invert dark:invert-0" />
         </Link>
