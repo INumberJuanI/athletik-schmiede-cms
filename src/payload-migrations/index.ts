8fd648aeca3028941602c536e941c4444cb8c84c
Juan Canales <41006075+INumberJuanI@users.noreply.github.com>
2025-10-19 16:59:55 +0000

    Add initial Payload migrations

--- src/payload-migrations/index.ts
+++ src/payload-migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20251019_165217 from './20251019_165217';
+import * as migration_20251019_165951 from './20251019_165951';
 
 export const migrations = [
   {
     up: migration_20251019_165217.up,
     down: migration_20251019_165217.down,
-    name: '20251019_165217'
+    name: '20251019_165217',
+  },
+  {
+    up: migration_20251019_165951.up,
+    down: migration_20251019_165951.down,
+    name: '20251019_165951'
   },
 ];
